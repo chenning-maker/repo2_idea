ebf4a95963852b66eba97a7155923dfe9bcf5b8f
chenning-maker <2137242118@qq.com>
2020-06-03 09:59:23 +0800

    修改TestGit

--- git/src/com/itheima/git/TestGit.java
+++ git/src/com/itheima/git/TestGit.java
@@ -6,9 +6,16 @@ public class TestGit {
     public static void main(String[] args) {
 
         System.out.println("Hello git!");
+        //Hello git!
     }
 
 
+ public void test(){
+        int i=0;
+        i +=10;
+     System.out.println(i);
+
+ }
 
 
 
